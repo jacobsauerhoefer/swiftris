5bf59d48b363c5ab5a1a75c364b7bb81fdc80a24
Jacob John <jacobsauerhoefer@gmail.com>
2016-07-30 21:03:44 -0400

    feat: setting up fall ticks for blocks

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -8,7 +8,23 @@
 
 import SpriteKit
 
+// #1
+
+let TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
+    
+    
+// #2
+    
+    var tick:(() -> ())?
+    
+    var tickLengthMillis = TickLengthLevelOne
+    
+    var lastTick:NSDate?
+    
+    
+    
     
     required init(coder aDecoder: NSCoder) {
         
@@ -16,11 +32,17 @@ class GameScene: SKScene {
         
     }
     
+    
+    
     override init(size: CGSize) {
         
         super.init(size: size)
         
+        
+        
         anchorPoint = CGPoint(x: 0, y: 1.0)
+        
+        
         
         let background = SKSpriteNode(imageNamed: "background")
         
@@ -34,5 +56,40 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+// #3
+        
+        guard let lastTick = lastTick else {
+            
+            return
+            
+        }
+        
+        let timePassed = lastTick.setIntervalSinceNow * -1000.0
+        
+        if timePassed > tickLengthMillis {
+            
+            self.lastTick = NSDate()
+            
+            tick?()
+            
+        }
+        
     }
+    
+    
+// #4
+    
+    func startTicking() {
+        
+        lastTick = NSDate()
+        
+    }
+    
+    func stopTicking() {
+        
+        lastTick = nil
+        
+    }
+    
 }
